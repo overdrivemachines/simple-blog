89cd7524eb7020d6e7c6a7824e1a844b8ca4e8a6
Dipen Chauhan <get.dipen@gmail.com>
2020-10-30 22:52:58 -0700

    Defined association with elements

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id           :integer          not null, primary key
+#  title        :string
+#  description  :text
+#  published    :boolean
+#  published_at :datetime
+#  author_id    :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Post < ApplicationRecord
   belongs_to :author
+  has_many :elements
 end
