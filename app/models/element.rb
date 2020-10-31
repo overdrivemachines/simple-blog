e2c6fae089ee8c1041b47919b990de7d1e643326
Dipen Chauhan <get.dipen@gmail.com>
2020-10-30 22:52:37 -0700

    Element_type can only be para, img or video

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: elements
+#
+#  id           :integer          not null, primary key
+#  element_type :string
+#  content      :text
+#  post_id      :integer          not null
+#  position     :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Element < ApplicationRecord
   belongs_to :post
+
+  validates :element_type, inclusion: { in: ['paragraph', 'image', 'video-embed']}
 end
