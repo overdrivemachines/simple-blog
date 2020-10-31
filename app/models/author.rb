fee73d7af618231a88724878cac1985164eab50c
Dipen Chauhan <get.dipen@gmail.com>
2020-10-30 22:53:14 -0700

    Annotated

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 class Author < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
