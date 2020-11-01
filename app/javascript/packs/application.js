b80800b1557a611d8c970c1792b296b060361984
Dipen Chauhan <get.dipen@gmail.com>
2020-11-01 13:40:38 -0800

    Added click event listener for paragraph content and cancel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,24 @@ require("channels")
 
 require("trix")
 require("@rails/actiontext")
+
+document.addEventListener('turbolinks:load', () = {
+
+  document.addEventListener('click', () = {
+    let element = event.target.closest('.paragraph-content');
+    if (!element) return;
+
+    element.classList.add('d-none');
+    element.nextElementSibling.classList.remove('d-none');
+  });
+
+  document.addEventListener('click', () = {
+    if (!event.target.matches('.cancel')) return;
+
+    let element = event.target.closest('.paragraph-form');
+
+    element.classList.add('d-none');
+    element.previousElementSibling.classList.remove('d-none');
+  });
+
+});
