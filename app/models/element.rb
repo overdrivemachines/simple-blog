30a7d60cab359c51b5a0a0c16e578626bf93c4e4
Dipen Chauhan <get.dipen@gmail.com>
2020-11-03 23:53:04 -0800

    Added paragraph? and image?

--- app/models/element.rb
+++ app/models/element.rb
@@ -15,4 +15,13 @@ class Element < ApplicationRecord
 
   validates :element_type, inclusion: { in: ['paragraph', 'image', 'video-embed']}
   has_rich_text :content
+
+  def paragraph?
+  	element_type == 'paragraph'
+  end
+
+  def image?
+  	element_type == 'image'
+  end
+
 end
