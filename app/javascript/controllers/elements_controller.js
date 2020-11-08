e9fb14fb91e4f134e1a1e909189ed15b6d94a26f
Dipen Chauhan <get.dipen@gmail.com>
2020-11-07 21:30:39 -0800

    Getting the id and positions of elements

--- app/javascript/controllers/elements_controller.js
+++ app/javascript/controllers/elements_controller.js
@@ -3,5 +3,11 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   sort() {
     console.log("Sorting elements...");
+    let element = document.getElementById('elements');
+    let element_items = Array.from(document.getElementsByClassName("element-item"));
+    let elements = element_items.map((element, index) => {
+      return ({id: element.dataset.id, position: index + 1});
+    });
+    console.log(elements);
   }
 }
